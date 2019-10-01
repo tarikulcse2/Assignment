9e32fcdb693ac0c053f33f54bade1728d939e444
unknown <tarikulcse2@gmail.com>
2019-10-01 08:05:19 +0600

    add search

--- Client/src/app/components/calculation/calculation.component.ts
+++ Client/src/app/components/calculation/calculation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { CalculationService } from 'src/app/services/calculation.service';
 import { ICalculationViewModel } from 'src/app/ViewModel/calculationViewModel';
 
@@ -9,11 +10,23 @@ import { ICalculationViewModel } from 'src/app/ViewModel/calculationViewModel';
 })
 export class CalculationComponent implements OnInit {
   public calculations: ICalculationViewModel[] = [];
+  public allCalculations: ICalculationViewModel[] = [];
+  public username = new FormControl();
   constructor(private calculationService: CalculationService) { }
 
   ngOnInit() {
-    this.calculationService.getCalculation().subscribe(res =>
-      this.calculations = res as ICalculationViewModel[] || []);
+    this.calculationService.getCalculation().subscribe(res => {
+      this.calculations = res as ICalculationViewModel[] || [];
+      this.allCalculations = res as ICalculationViewModel[] || [];
+    }
+    );
+  }
+  searchUser(value: string) {
+    this.calculations = this.allCalculations.filter(s => s.userName === value);
   }
 
+  searchDate(sD: string, eD: string) {
+    this.calculations = this.allCalculations.filter(s =>
+      new Date(s.dateOfCalculation) <= new Date(sD) && new Date(s.dateOfCalculation) >= new Date(eD));
+  }
 }
